b3fb506b4cbe79d6da96d9b674c38ca8cbae0176
Chirag Shankar <chishankar@gmail.com>
2019-03-26 13:16:14 -0400

    added new things

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,11 +2,11 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
-import styles from './Navbar.css';
+import styles from './Home.css';
 
 type Props = {};
 
-export default class Home extends Component<Props> {
+export default class Navbar extends Component<Props> {
   props: Props;
 
   render() {
