55d60328d07add362d54dca8fc781bb7a12afcac
Akbar Baig <anbaig@umd.edu>
2019-04-04 16:35:47 -0400

    changed http value to placeholder instead

--- app/components/UrlSearch.js
+++ app/components/UrlSearch.js
@@ -59,7 +59,7 @@ export default class UrlSearch extends Component<Props>{
     const notValidateHTML = <i float="right" className="fas fa-check" className={styles.fauncheck}></i>;
       return(
         <div>
-          <input type="text" placeholder="Google Search.." value="https://" onKeyUp={this.handleEnter} onChange={this.handleInput}/>
+          <input type="text" placeholder="https://<website>" onKeyUp={this.handleEnter} onChange={this.handleInput}/>
           {showValidate && validateHTML}
           {!showValidate && notValidateHTML}
         </div>
