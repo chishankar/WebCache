70c23fccce79fb2c08653e08056a8d238a494727
Akbar Baig <anbaig@umd.edu>
2019-04-07 20:54:03 -0400

    fixed weird merge stuff

--- app/actions/urlsearch.js
+++ app/actions/urlsearch.js
@@ -3,12 +3,7 @@ import type { GetState, Dispatch } from '../reducers/types'; //what to change in
 export const changeActiveUrl = (activeUrl) => {
   console.log("dispatching: "+ activeUrl)
   return {
-<<<<<<< HEAD
       type: "URLUPDATE",
       activeUrl
-=======
-      type: 'URL_UPDATE',
-      activeUrl: activeUrl
->>>>>>> 3671a08563fd0594b6c41646adf5fb4befa11357
   }
 }
