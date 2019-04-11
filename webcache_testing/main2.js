b92809c67f8e7f262e5a2bd5b1f171ba301c731d
cblair28 <cblair28@terpmail.umd.edu>
2019-04-11 19:57:29 -0400

    index updated - search

--- webcache_testing/main2.js
+++ webcache_testing/main2.js
@@ -116,86 +116,110 @@ function addFilesToMainIndex(fileNames, mainIndex) {
   });
 }
 
+
+
+function getWordLocs(codes) {
+  wordLocs = []
+  codes.forEach( code => {
+    locStart = 1+parseInt(code.toString().slice(0,1))
+    id = parseInt(code.toString().slice(1,locStart));
+    fn = lookup[id].fileName;
+    loc = parseInt(code.toString().slice(locStart))
+
+    fileInd = wordLocs.findIndex(file => file.fileName == fn);
+
+    if (fileInd > 0) {
+      wordLocs[fileInd].fileLocs.push(loc);
+    } else {
+      wordLocs.push({
+        fileName: fn,
+        fileLocs: [loc]
+      });
+    }
+  });
+  return wordLocs;
+}
+
 function search(searchStr, index) {
-  let finalResults = []
+  let finalResults = [];
   let fileLists = []; // will hold all files for each search word
   let wordResults = [];
   let reducedFileList = []; // will hold all files that contain all search words
   let searchWords = searchStr.toLowerCase().trim().split(/\s+/);
   let startIndex = 0;
-  let singleRes = [];
 
   // populate fileLists with lists of files holding each word.
   searchWords.forEach(word => {
     let wordFiles = [];
-    let results = _.findWhere(index, {word: word})
-    wordResults.push(results.allLocs);
-    if (searchWords.length <= 1) {
-      singleRes = results.allLocs;
-      return;
-    }
-    results.allLocs.forEach(wordLocs => {
-      wordFiles.push(wordLocs.fileName);
+    results = _.findWhere(index, {word: word});
+    wordLocs = getWordLocs(results.allLocs);
+    wordResults.push(wordLocs);
+    wordLocs.forEach(fileLocs => {
+      wordFiles.push(fileLocs.fileName);
     });
-
     fileLists.push(wordFiles);
   });
 
   if (searchWords.length <= 1) {
-    return singleRes;
+    wordResults.forEach(obj => {
+      finalResults.push({
+        fileName: obj.fileName,
+        locations: obj.fileLocs
+      });
+    })
+
+    return finalResults;
   }
 
   //  PART 1: Get list of files that contain all search words
 
-    let lastFileInds = [];
+  let lastFileInds = [];
 
-    fileLists.forEach(wordFiles => {
-      wordFiles.sort();
-      lastFileInds.push(0);
-    });
+  fileLists.forEach(wordFiles => {
+    wordFiles.sort();
+    lastFileInds.push(0);
+  });
 
-    let wordNum = 0;
-    let numWords = searchWords.length;
-    let listReduced = false;
-    while(listReduced === false) {
+  let wordNum = 0;
+  let numWords = searchWords.length;
+  let listReduced = false;
 
-      if (lastFileInds[0] === fileLists[0].length) {
+  while(listReduced === false) {
+
+    if (lastFileInds[0] === fileLists[0].length) {
+      break;
+    }
+
+    let currFile = fileLists[0][lastFileInds[0]];
+    wordNum = 1;
+    while(wordNum > 0) {
+      // if end of a list reached, reducedFileList done.
+      if (lastFileInds[wordNum] === fileLists[wordNum].length) {
+        listReduced = true;
         break;
       }
 
-      let currFile = fileLists[0][lastFileInds[0]];
-      wordNum = 1;
+      if (fileLists[wordNum][lastFileInds[wordNum]] < currFile) {
+        lastFileInds[wordNum]++;
+      }
 
-        while(wordNum > 0) {
-          // if end of a list reached, reducedFileList done.
-          if (lastFileInds[wordNum] === fileLists[wordNum].length) {
-            listReduced = true;
-            break;
-          }
+      else if (fileLists[wordNum][lastFileInds[wordNum]] > currFile) {
+        // backtrack
+        lastFileInds[--wordNum]++;
+      }
 
-          if (fileLists[wordNum][lastFileInds[wordNum]] < currFile) {
-            lastFileInds[wordNum]++;
-          }
-
-          else if (fileLists[wordNum][lastFileInds[wordNum]] > currFile) {
-            // backtrack
-            lastFileInds[--wordNum]++;
-          }
-
-          else {
-            if (wordNum = searchWords.length - 1) {
-              reducedFileList.push(currFile);
-              lastFileInds[0]++;
-              wordNum = 0;
-            }
-            else {
-              wordNum++
-            }
-          }
+      else {
+        if (wordNum = searchWords.length - 1) {
+          reducedFileList.push(currFile);
+          lastFileInds[0]++;
+          wordNum = 0;
         }
-
+        else {
+          wordNum++
+        }
+      }
     }
-
+  }
 
   // PART 2: Find search string locations in each file
 
@@ -216,8 +240,6 @@ function search(searchStr, index) {
 
       wordLocs.push(_.findWhere(wordResults[i], {fileName: fileName}).fileLocs.sort(function(a, b){return a-b}));
     }
-
-
 
     let wordNum = 0;
     let searchComplete = false;
@@ -328,9 +350,10 @@ var rl = readline.createInterface({
   //  lookup.forEach( fileWord => {
   //   console.log(fileWord.fileName + " " + fileWord.ID + "\n");
   //  });
-    mainIndex.forEach( fileWord => {
-    console.log(fileWord.word + ":\n" + fileWord.allLocs + "\n");
-  });
+    // mainIndex.forEach( fileWord => {
+    // console.log(fileWord.word + ":\n" + fileWord.allLocs + "\n");
+    user_search("has the");
+ // });
  });
 
 
