26b5eed0a2b6259eb9c4eafda3e08be3fe67f230
Akbar Baig <anbaig@umd.edu>
2019-04-19 14:00:32 -0400

    Added ability to save annotations and recall them

--- app/components/RenderText.js
+++ app/components/RenderText.js
@@ -5,18 +5,18 @@ import webview from 'electron';
 import styles from './Text.css';
 import HighlightText from './HighlightText';
 import 'react-notifications/lib/notifications.css';
-import {NotificationContainer, NotificationManager} from 'react-notifications';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 import * as resourcePath from '../utilities/ResourcePaths';
 
 const fs = require('fs');
 
 // Returns fulle path needed for iFrame
-function getResourceBuilder(path){
+function getResourceBuilder(path) {
   return new resourcePath.ResourcePaths(path).getFullPath();
 }
 
 // Returns the base resource
-function getResourcePath(path){
+function getResourcePath(path) {
   return new resourcePath.ResourcePaths(path).getResourceDir();
 }
 
@@ -30,7 +30,7 @@ function getRenderText(filePath, iframeRef) {
   var resourceHtml = fs.readFileSync(resource).toString();
   var injectScript = fs.readFileSync(jsResource).toString();
 
-  resourceHtml += "<script>" + injectScript + "<\/script>";
+  resourceHtml += "<script id=\"webcache-script\">" + injectScript + "<\/script>";
 
   // change all paths to become relative
   resourceHtml = resourceHtml.replace(/href="/g, "href=\"" + resourceDir + "/");
@@ -45,49 +45,59 @@ type Props = {
   color: string
 }
 
-export default class RenderText extends Component<Props> {
+export default class RenderText extends Component < Props > {
   props: Props;
 
-  constructor(props){
+  constructor(props) {
     super(props);
     this.iframeRef = React.createRef();
   }
 
   // Once the component mounts, add an event listener to listen for messages and pass all the messages to the handleIFrameTask
-  componentDidMount(){
-    window.addEventListener('message',this.handleIFrameTask)
+  componentDidMount() {
+    window.addEventListener('message', this.handleIFrameTask)
     // window.postMessage(data,'*')
   }
 
   // Upon URL change, change the URL
-  componentDidUpdate(prevProps){
-      console.log("updating data source for iframe")
-      // window.postMessage(data,'*')
+  componentDidUpdate(prevProps) {
+    console.log("updating data source for iframe")
+    // window.postMessage(data,'*')
   }
 
   // Takes in data returned by window.postMessage from the iframe rendered within the component
   handleIFrameTask = (e) => {
 
-    if (e.data == 'clicked button'){
+    if (e.data == 'clicked button') {
 
-    } else if (e.data == 'highlighted text'){
+    } else if (e.data == 'highlighted text') {
 
-      let data = {color: this.props.color};
-      window.postMessage(data,'*');
+      let data = { color: this.props.color };
+      window.postMessage(data, '*');
 
-    } else if (e.data.savedData){
-
-      console.log(e.data.savedData);
-
+    } else if (e.data.savedData) {
+      let updatedHtmlWScript = e.data.savedData;
+      var end = updatedHtmlWScript.indexOf("<script id=\"webcache-script\">"); 
+      let updatedHtml = updatedHtmlWScript.substring(0, end - 1); //remove our injected script tag from the document
+      //delete the previous version of the file
+      fs.unlink(this.props.activeUrl, (err) => {
+        if (err) throw err;
+        //console.log(this.props.activeUrl + 'was deleted');
+      });
+      // re write the current version of the html page
+      fs.writeFile(this.props.activeUrl, updatedHtml, (err) => {
+        // throws an error, you could also catch it here
+        if (err) throw err;
+        // success case, the file was saved
+        //console.log(this.props.activeUrl + 'saved!');
+      });
     }
   }
 
   // Function to handle saving data
   handleSaveTask = () => {
-
-    let data = {save: "save"}
+    let data = { save: "save" }
     window.postMessage(data, '*');
-
   }
 
   render() {
