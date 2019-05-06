9d430192680547693b56e8e76d04dde3baf48945
Chirag Shankar <chishankar@gmail.com>
2019-05-05 22:05:57 -0400

    added more comments and documentation

--- app/actions/sidebar.js
+++ app/actions/sidebar.js
@@ -1,39 +1,63 @@
 import type { GetState, Dispatch } from '../reducers/types';
-
-export const addHighlight = (highlightData) => {
+/**
+ * Adds highlight to annotations
+ * @param  {Object} highlightData
+ * @returns HIGHLIGHT action
+ */
+export const addHighlight = (highlightData: Object) => {
   return {
       type: "HIGHLIGHT",
       highlightData
   }
 }
-
-export const addComment = (commentData) => {
+/**
+ * Adds comment to highlight
+ * @param  {Object} commentData
+ * @returns COMMENT action
+ */
+export const addComment = (commentData: Object) => {
   return {
       type: "COMMENT",
       commentData
   }
 }
-
-export const deleteHighlights = (deleteId) => {
+/**
+ * Sends delete request
+ * @param  {String} deleteId
+ * @returns DELETE action
+ */
+export const deleteHighlights = (deleteId: String) => {
   return {
       type: "DELETE",
       deleteId
   }
 }
 
-export const viewHighlight = (id) => {
+/**
+ * Tells which highlight id should be in view
+ * @param  {String} id
+ * @returns VIEW action
+ */
+export const viewHighlight = (id: String) => {
   return {
       type: "VIEW",
       id
   }
 }
-
+/**
+ * Switches boolean to hide all annotations on a page
+ * @returns HIDEHIGHLIGHTS action
+ */
 export const hideHighlights = () => {
   return {
       type: "HIDEHIGHLIGHTS"
     }
 }
 
+/**
+ * Resets annotations list for new page
+ * @returns CLEAR action
+ */
 export const clearHighlights = () => {
   return {
       type: "CLEAR"
--- app/actions/urlsearch.js
+++ app/actions/urlsearch.js
@@ -1,6 +1,10 @@
 import type { GetState, Dispatch } from '../reducers/types'; //what to change in reducers/types
-
-export const changeActiveUrl = (activeUrl) => {
+/**
+ * Actions to update active url in the store
+ * @param  {String} activeUrl
+ * @returns URLUPDATE action
+ */
+export const changeActiveUrl = (activeUrl: String) => {
   return {
       type: "URLUPDATE",
       activeUrl
